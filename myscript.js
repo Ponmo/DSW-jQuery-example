8de591007766230db123f7e972d845ed07ef9529
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-01-28 10:02:20 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	$("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
+		$("th").css("background-color", "pink");
+		$("td").css("background-color", "pink");
 	});
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
@@ -11,5 +13,11 @@ $(document).ready(function() {
 	});
 	$("p").click(function(){
 		$(this).css("background-color", "lightblue");
+	});
+	$("th").click(function(){
+		$(this).css("background-color", "grey");
+	});
+	$("td").click(function(){
+		$(this).css("background-color", "grey");
 	});
 });
